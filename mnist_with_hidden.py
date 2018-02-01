51e03196808904cdca02f9f3da4955a4edab9011
Kumar Surisetti <surisett@mathlab.utsc.utoronto.ca>
2018-02-01 13:19:49 -0500

    changed mnist_with_hidden

--- mnist_with_hidden.py
+++ mnist_with_hidden.py
@@ -8,9 +8,9 @@ mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
 #plt.imshow(mnist.train.images[100,:].reshape(28,28),cmap='gist_gray');plt.show()
 
 #total number of layers
-num_layers=3
+num_layers=4
 #no. of units in each layer
-units=[784,1000,1000,10]
+units=[784,1000,1000,1000,10]
 
 #placeholders
 x=tf.placeholder(tf.float32,shape=[None,784])
@@ -22,15 +22,18 @@ W2=tf.Variable(tf.random_normal([units[1],units[2]]))
 b2=tf.Variable(tf.random_normal([units[2]]))
 W3=tf.Variable(tf.random_normal([units[2],units[3]]))
 b3=tf.Variable(tf.random_normal([units[3]]))
+W4=tf.Variable(tf.random_normal([units[3],units[4]]))
+b4=tf.Variable(tf.random_normal([units[4]]))
 
 #graph operations
 y1=tf.add(tf.matmul(x,W1),b1)
 y2=tf.add(tf.matmul(y1,W2),b2)
-y=tf.matmul(y2,W3)+b3
+y3=tf.add(tf.matmul(y2,W3),b3)
+y=tf.matmul(y3,W4)+b4
 #loss function
 cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true,logits=y))
 #optimizer
-optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.001)
+optimizer=tf.train.AdamOptimizer(learning_rate=0.0001)
 train=optimizer.minimize(cross_entropy)
 #session
 init=tf.global_variables_initializer()
@@ -38,12 +41,18 @@ init=tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
 
-    for step in range(900):
-        batch_x,batch_y=mnist.train.next_batch(100)
+    for step in range(10000):
+        batch_x,batch_y=mnist.train.next_batch(10)
         sess.run(train,feed_dict={x:batch_x,y_true:batch_y})
-    #evaluation
-    correct_prediction=tf.equal(tf.argmax(y,1),tf.argmax(y_true,1)) #THIS IS A NEW NODE
-    acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32)) #SO IS THIS
-    print(sess.run(acc,feed_dict={x:mnist.test.images,y_true:mnist.test.labels}))
-    print('\n')
-    print(sess.run(acc,feed_dict={x:mnist.train.images,y_true:mnist.train.labels}))
+
+        if (step%100)==True:
+            #evaluation
+            correct_prediction=tf.equal(tf.argmax(y,1),tf.argmax(y_true,1)) #THIS IS A NEW NODE
+            acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32)) #SO IS THIS
+            print(sess.run(acc,feed_dict={x:mnist.test.images,y_true:mnist.test.labels}))
+            #print('\n')
+            #print(sess.run(acc,feed_dict={x:mnist.train.images,y_true:mnist.train.labels}))
+    
+            
+
+    
